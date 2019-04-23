26a6d80e284912e65a62075eb9302acb7e4768a0
Matsievskyy <taras.matsievskyy1@gmail.com>
2019-04-24 02:54:27 +0300

    done

---
+++ script.js
@@ -0,0 +1,24 @@
+const me = {
+    name : 'Taras',
+    secondname : 'Matsievskyy',
+    dateOfBirth : '01.10',
+    age : '27',
+    from : 'Ukraine',
+    nationality : 'ukrainian',
+    job : 'manager',
+    marritalStatus : 'single',
+    addres : 'Mayorivka str.',
+    town : 'Lviv',
+    favoriteBook : 'Atlas Shrugged',
+    favoriteBand : 'The Score',
+    favoriteSong : 'Sam Tinnesz - Play With Fire',
+    favoriteTvShow : 'Peaky Blinders',
+    favoriteMovie : 'The Lord of the Rings',
+    favoriteGame : 'Witcher',
+    favoriteSport : 'Football',
+    favoriteClub : 'Real Madrid',
+    favoritePlayer : 'Zinedine Zidane',
+    favoriteCoach : 'Zinedine Zidane',
+}
+
+console.log(me);
